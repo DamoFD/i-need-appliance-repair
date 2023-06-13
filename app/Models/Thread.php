32f1fb9a7468619b477c2771babef8b9384e24a0
DamoFD <voshall@yahoo.com>
2023-06-12 20:32:59 -0700

    tag migrations

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasTags;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,7 +10,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Thread extends Model
 {
     use HasFactory;
-
+    use HasTags;
+    
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
