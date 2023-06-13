47b1ba2b57802bdbb5ee5a9b90cbaa6016c379b6
DamoFD <voshall@yahoo.com>
2023-06-12 19:08:23 -0700

    sidebar routing

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\IsAdmin;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        return $this->middleware([IsAdmin::class, Authenticate::class]);
+    }
+
     /**
      * Display a listing of the resource.
      *
